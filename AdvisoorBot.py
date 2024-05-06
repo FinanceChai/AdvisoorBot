af8baa6b17db5a08c22d4d00db8ddc5cd3e8db17
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-06 17:46:58 +0100

    Update AdvisoorBot.py
    x

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -3,7 +3,7 @@ import asyncio
 import random
 import requests
 from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from dotenv import load_dotenv
 
 # Set the event loop policy to prevent issues on Windows environments
