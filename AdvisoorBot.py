ffb152f070df38a0d47f360102950139140a630f
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-06 18:50:12 +0100

    Update AdvisoorBot.py
    c

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -95,7 +95,7 @@ def button_handler(update: Update, context: CallbackContext):
 def main():
     """Main function to initialize the bot and handle polling."""
     bot = Bot(token=TELEGRAM_TOKEN)
-    updater = Updater(bot=bot, use_context=True)
+    updater = Updater(bot=bot)
     
     # Adding command and callback query handlers
     updater.dispatcher.add_handler(CommandHandler('start', start))
