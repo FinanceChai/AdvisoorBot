50f9a20dbbf60caa58500a548667ef430e51e0c7
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-07 11:17:23 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -4,19 +4,24 @@ import random
 import requests
 from telegram import Bot
 from dotenv import load_dotenv
-from urllib.parse import quote as safely_quote  # Import quote function for URL encoding
+from urllib.parse import quote as safely_quote
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Retrieve environment variables
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 SOLSCAN_API_KEY = os.getenv('SOLSCAN_API_KEY')
 TARGET_ADDRESSES = os.getenv('TARGET_ADDRESS', '').split(',')
 JUPITER_REFERRAL_KEY = os.getenv('JUPITER_REFERRAL_KEY')
 IMAGE_DIRECTORY = os.path.abspath('/root/main/AdvisoorBot/memes')
-excluded_symbols = {"WSOL", "SOL", "USDC", "WAVAX", "WBTC", "WETH", "ETH", "BONK"}
+
+def get_random_image_path(image_directory):
+    """Returns a random image path from the specified directory."""
+    images = [os.path.join(image_directory, file) for file in os.listdir(image_directory) if file.endswith(('.png', '.jpg', '.jpeg'))]
+    if images:
+        return random.choice(images)
+    else:
+        return None
 
 
 async def main():
