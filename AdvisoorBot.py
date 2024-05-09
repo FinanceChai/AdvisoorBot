2474a583cf9c809c06cc3a42892842e9bac7ff04
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-09 13:14:30 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -11,7 +11,7 @@ load_dotenv()
 SOLSCAN_API_KEY = os.getenv("SOLSCAN_API_KEY")
 
 async def fetch_token_metadata(session, token_address):
-    url = f"https://pro-api.solscan.io/v1.0/token/list?mintAddress={quote(token_address)}&limit=1"
+    url = f"https://pro-api.solscan.io/v1.0/token/list?mintAddress={token_address}&limit=1"
     headers = {'accept': '*/*', 'token': SOLSCAN_API_KEY}
     async with session.get(url, headers=headers) as response:
         if response.status == 200:
