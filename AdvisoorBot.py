617831e90f2fe67d8b2c26c77e3bb4c41d27f248
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-09 13:04:28 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -1,44 +1,37 @@
 import aiohttp
-from dotenv import load_dotenv
+import asyncio
 import os
 
-load_dotenv()
-
+# Load API key from environment variable
 SOLSCAN_API_KEY = os.getenv("SOLSCAN_API_KEY")
 
-async def fetch_market_cap(session, token_address):
+async def fetch_token_metadata(session, token_address):
     url = f"https://pro-api.solscan.io/v1.0/token/meta?tokenAddress={token_address}"
     headers = {'accept': '*/*', 'token': SOLSCAN_API_KEY}
     
     async with session.get(url, headers=headers) as response:
         if response.status == 200:
             data = await response.json()
-            price = data.get('priceUst', None)
-            token_name = data.get('tokenName', 'Unknown')
-            token_symbol = data.get('tokenSymbol', 'Unknown')
-            market_cap = data.get('marketCapFD', None)
-                        
-            print(f"Price: {price}, Type: {type(price)}")
-            print(f"Name: {token_name}, Type: {type(token_name)}")
-            print(f"Token Symbol: {token_symbol}, Type: {type(token_symbol)}")
-            print(f"Market Cap: {market_cap}, Type: {type(market_cap)}")
-            
-            return token_name, token_symbol, market_cap
-        
-        print(f"Failed to fetch data. Status code: {response.status}")
-        return "Unknown", "Unknown", None
-
+            token_data = data.get('data', {})
+            if token_data:
+                token_info = token_data[0]
+                name = token_info.get('tokenName', 'Unknown')
+                symbol = token_info.get('tokenSymbol', 'Unknown')
+                market_cap = token_info.get('marketCapFD', 'Unknown')
+                return name, symbol, market_cap
+            else:
+                print("No token data found.")
+        else:
+            print(f"Failed to fetch data. Status code: {response.status}")
+        return "Unknown", "Unknown", "Unknown"
 
 async def main():
     token_address = "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"  # Example token address
     async with aiohttp.ClientSession() as session:
-        data = await fetch_market_cap(session, token_address)
-        if data is not None:
-            token_name, token_symbol, market_cap = data
-            print(f"Token Name: {token_name}")
-            print(f"Token Symbol: {token_symbol}")
-            print(f"Market Cap: {market_cap}")
+        name, symbol, market_cap = await fetch_token_metadata(session, token_address)
+        print(f"Token Name: {name}")
+        print(f"Token Symbol: {symbol}")
+        print(f"Market Cap: {market_cap}")
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
