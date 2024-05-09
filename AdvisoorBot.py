e1d334fad2cb296ad7df2fa9b925061f01713fe0
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-09 14:37:56 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -10,7 +10,7 @@ load_dotenv()
 SOLSCAN_API_KEY = os.getenv("SOLSCAN_API_KEY")
 
 async def fetch_token_metadata(session, token_address):
-    url = f"https://pro-api.solscan.io/v1.0/token/meta?tokenAddress={token_address}"
+    url = f"https://pro-api.solscan.io/v1.0/market/token{token_address}?limit=2"
     headers = {'accept': '*/*'}
 
     # Add API key to headers if available
