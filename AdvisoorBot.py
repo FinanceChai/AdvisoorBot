f4791d32f683e3129adeb65139e005d7f40f67ea
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-09 12:58:06 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -7,41 +7,25 @@ load_dotenv()
 SOLSCAN_API_KEY = os.getenv("SOLSCAN_API_KEY")
 
 async def fetch_market_cap(session, token_address):
-    url = f"https://pro-api.solscan.io/v1.0/token/meta?tokenAddress={token_address}"
+    url = f"https://pro-api.solscan.io/v1.0/token/TokenList={token_address}"
     headers = {'accept': '*/*', 'token': SOLSCAN_API_KEY}
     
     async with session.get(url, headers=headers) as response:
         if response.status == 200:
             data = await response.json()
-            price = data.get('price', None)
-            token_name = data.get('name', 'Unknown')
-            token_symbol = data.get('symbol', 'Unknown')
-            decimals = data.get('decimals', None)
-            supply = data.get('supply', None)
-            
-            print(f"Price: {price}, Type: {type(price)}")
-            print(f"Supply: {supply}, Type: {type(supply)}")
-            print(f"Decimals: {decimals}, Type: {type(decimals)}")
-            
-            if price is not None and decimals is not None and supply is not None:
-                try:
-                    supply = int(supply)  # Convert to integer
-                    market_cap = price * (supply / decimals)
-                    formatted_price = f"${market_cap:,.2f}"
-                    return token_name, token_symbol, formatted_price
-                except ZeroDivisionError:
-                    print("Error: Decimals cannot be zero.")
-            else:
-                print("Market cap data is missing or invalid.")
-    return "Unknown", "Unknown", None
+            price = data.get('priceUst', None)
+            token_name = data.get('tokenName', 'Unknown')
+            token_symbol = data.get('tokenSymbol', 'Unknown')
+            market_cap = data.get('marketCapFD', None)
+                        
+            print(f"Price: {price}, Type: {type(priceUst)}")
+            print(f"Name: {token_name}, Type: {type(tokenName)}")
+            print(f"token_symbol: {token_symbol}, Type: {type(tokenSymbol)}")
 
 async def main():
     token_address = "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"  # Example token address
     async with aiohttp.ClientSession() as session:
         token_name, token_symbol, market_cap = await fetch_market_cap(session, token_address)
-        print(f"Token Name: {token_name}")
-        print(f"Token Symbol: {token_symbol}")
-        print(f"Market Cap: {market_cap}")
 
 if __name__ == "__main__":
     import asyncio
