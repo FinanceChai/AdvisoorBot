660b3c9f3261de589eae4587de8f30a14a6f5666
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-14 19:49:42 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -104,7 +104,7 @@ async def create_message(session, transactions):
         
         # Check if metadata was successfully fetched
         if not token_metadata:
-            message_lines.append("Failed to fetch ca {safely_quote(transaction['token_address'])}. Might be a future launch!\n")
+            message_lines.append("Failed to fetch ca {token_address}. Maybe be a future launch!?\n")
             continue
         
         # Extract token details with default values if keys are missing
