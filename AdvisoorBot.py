6859fa22f10f198a7ba1fd3bc0c7a57ddb02aea7
FinanceChai <50843817+FinanceChai@users.noreply.github.com>
2024-05-17 13:24:48 +0100

    Update AdvisoorBot.py

--- AdvisoorBot.py
+++ AdvisoorBot.py
@@ -104,8 +104,8 @@ async def create_message(session, transactions):
 
         message_lines.append(
             f"Ticker: {ticker}\n"
-            f"🤓 Solscan: <a href='https://solscan.io/token/{safely_quote(transaction['token_address'])}'>Token</a> (-{last_five_chars_token})"
-            f"<a href='https://solscan.io/account/{safely_quote(transaction['owner_address'])}'>Buyer Wallet</a> (-{last_five_chars_owner})\n"
+            f"🤓 Solscan: <a href='https://solscan.io/token/{safely_quote(transaction['token_address'])}'>Token </a> (-{last_five_chars_token})"
+            f"- <a href='https://solscan.io/account/{safely_quote(transaction['owner_address'])}'>Buyer Wallet</a> (-{last_five_chars_owner})\n"
             f"<a href='https://dexscreener.com/search?q={safely_quote(transaction['token_address'])}'>DexScreener🔍 | </a>"
             f"<a href='https://rugcheck.xyz/tokens/{safely_quote(transaction['token_address'])}'>RugCheck✅</a>\n"
         )
